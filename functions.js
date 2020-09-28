318422d338c5dbb6c9816118c68fea4e71d7bfbc
Daniel Hensler <henslerdl@gmail.com>
2020-09-28 09:19:09 -0600

    Added secondary button feature

--- functions.js
+++ functions.js
@@ -1,9 +1,12 @@
 let clickCount = 0
 let countEL = document.getElementById("count")
 
+let secondaryEl = document.getElementById("secondary")
+
 let doubled = false
 let peanutted = false
 let automated = false
+let seconded = false
 
 let startButtonEl = document.getElementById('start')
 let allEl = document.getElementById('all')
@@ -19,13 +22,15 @@ let peanutEl = document.getElementById('peanut')
 
 let autoEl = document.getElementById('auto')
 
+let secondEl = document.getElementById('second')
+
 function clicked() {
     clickCount++
     if (doubled) {
         clickCount++
     }
     let count = clickCount
-    console.log(`Button clicked ${clickCount} times`)
+    //console.log(`Button clicked ${clickCount} times`)
     if (count >= 1000) {
         countEL.innerHTML = `You have a lot of time on your hands, there is no way you need ${count} cookies`
     } else if (count >= 500) {
@@ -48,6 +53,17 @@ function clicked() {
     if (count >= 200 && !automated) {
         autoEl.classList.remove("removeA")
     }
+    if (count >= 50 && !seconded) {
+        secondEl.classList.remove("removeA")
+    }
+}
+
+function secClicked() {
+    if (seconded) {
+        clicked()
+        //console.log('clickedSec')
+    }
+    //console.log('secClicked')
 }
 
 function doubleIt() {
@@ -65,7 +81,7 @@ function peanutIt() {
         peanutted = true
         peanutEl.classList.add("removeA")
         clickCount -= 500
-        console.log("cookie changed")
+        //console.log("cookie changed")
         document.getElementById("start").src = "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcT1gOPMOsAUnlb7g8pIwjRpZdrMnDPVsS3IrQ&usqp=CAU"
     } else {
         window.alert("You need 500 cookies to purchase this upgrade.")
@@ -77,28 +93,40 @@ function automateIt() {
         automated = true
         autoEl.classList.add("removeA")
         clickCount-=200
-        console.log("Autoclicker engaged")
+        //console.log("Autoclicker engaged")
         let autoClick = setInterval(clicked, 1000)
     } else {
         window.alert("You need 200 cookies to purchase this upgrade.")
     }
 }
 
+function secondIt() {
+    if (clickCount >= 50) {
+        seconded = true
+        secondEl.classList.add("removeA")
+        secondaryEl.classList.remove("removeA")
+        clickCount-=50
+        //console.log("Secondary button activated")
+    } else {
+        window.alert("You need 50 cookies to purchase this upgrade.")
+    }
+}
+
 function shrink() {
     startButtonEl.classList.add('shrink')
     allEl.classList.add('flash')
-    console.log("Shrink")
+    //console.log("Shrink")
 }
 function grow() {
     startButtonEl.classList.remove('shrink')
     allEl.classList.remove('flash')
-    console.log("Grow")
+    //console.log("Grow")
 }
 function remove() {
     startButtonEl.classList.add("removeA")
     removeEl.classList.add("removeA")
     returnEl.classList.remove("removeA")
-    console.log("remove")
+    //console.log("remove")
 }
 function hideA() {
     startButtonEl.classList.add("hide")
@@ -120,6 +148,7 @@ startButtonEl.addEventListener('click', clicked)
 startButtonEl.addEventListener('mousedown', shrink)
 startButtonEl.addEventListener('mouseup', grow)
 
+document.addEventListener('keydown', secClicked)
 
 removeEl.addEventListener('click', remove)
 returnEl.addEventListener('click', returnA)
@@ -129,3 +158,4 @@ showEl.addEventListener('click', show)
 doubleEl.addEventListener('click', doubleIt)
 peanutEl.addEventListener('click', peanutIt)
 autoEl.addEventListener('click', automateIt)
+secondEl.addEventListener('click', secondIt)
